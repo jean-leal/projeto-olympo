0e9799c5abe4ba29964c881e9bdcc935b9e68c19
JLeal <jeanlds13@gmail.com>
2022-08-06 12:44:12 -0300

    atualizando a aplicação

--- index2.js
+++ index2.js
@@ -1,26 +1,17 @@
-const express = require ('express');
-const session = require ('express-session');
-const bodyParser = require ('body-parser');
 
-let path = require('path');
-const app = express();
 
-let login = 'admin';
-let password = '123';
 
-app.use(session({secret: 'sldkn1f3l2d6slnsnfskdnmflk'}));
-app.use(bodyParser.urlencoded({extended:true}));
-
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-app.use('/public', express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, '/views'));
-
-app.post('/', (req, res)=>{
-    console.log(req.body.login)
-    res.render('index2');
-})
-
-app.get('/', (req, res)=>{
-    res.reder('index2');
-})
+function entrar(){
+    
+    let login = '123';
+    let password = '123';
+    let usuario = document.getElementById('login').value
+    let senha = document.getElementById('password').value
+    
+    if ( usuario == login && senha == password){
+        window.location.href = "http://seusite.com"  
+    } else{
+        alert(usuario)
+    }
+    
+}
--- main.js
+++ main.js
@@ -2,40 +2,14 @@ const {app, BrowserWindow} = require('electron');
 
 //janela principal
 let mainWindow = null;
-async function createWindow() {
+function createWindow() {
     mainWindow = new BrowserWindow({
         width:800,
         height:600
     });
 
-    await mainWindow.loadFile('views/index.html');
+    mainWindow.loadFile('views/index.html');
 }
 // on ready
 app.whenReady().then(createWindow);
 //----------------------------------------------------------
-const express = require ('express');
-const session = require ('express-session');
-const bodyParser = require ('body-parser');
-
-let path = require('path');
-const exp = express();
-
-let login = 'admin';
-let password = '123';
-
-exp.use(session({secret: 'sldkn1f3l2d6slnsnfskdnmflk'}));
-exp.use(bodyParser.urlencoded({extended:true}));
-
-exp.engine('html', require('ejs').renderFile);
-exp.set('view engine', 'html');
-exp.use('/public', express.static(path.join(__dirname, 'public')));
-exp.set('views', path.join(__dirname, '/views'));
-
-exp.post('/', (req, res)=>{
-    console.log(req.body.login)
-    res.render('index2');
-})
-
-exp.get('/', (req, res)=>{
-    res.reder('index2');
-})
